cc6bf2ce4b2d443759617ba895eaa563bdf3ec9f
Anazz24 <analauramoraes87@gmail.com>
2022-11-25 08:51:54 -0300

    video 3 25-11

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -8,9 +8,24 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 })
 export class RegistroPage implements OnInit {
 
-  constructor() { }
+  formRegistro: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.formRegistro = this.formBuilder.group({
+      nome: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
+      cpf: ['', [Validators.required, Validators.minLength(11), Validators.maxLength(11)]],
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      senha: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(8)]],
+      confirmarSenha: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(8)]]
+    });
+
+   }
 
   ngOnInit() {
+  }
+
+  salvarRegistro(){
+    console.log('Formulário: ', this.formRegistro.valid);
   }
 
 }
